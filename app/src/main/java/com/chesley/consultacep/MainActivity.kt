716c7b91516037b3f75ba0551f17d23e7fe8a5db
wesleymenezesjanuario <wesley.menezes@invoicex.io>
2023-12-16 00:53:37 -0300

    Commit

---
+++ app/src/main/java/com/chesley/consultacep/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.chesley.consultacep
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
